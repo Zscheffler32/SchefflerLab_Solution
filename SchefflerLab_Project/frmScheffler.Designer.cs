f70e0a3e0a36aa008a66be136e8c5b9d0d251182
Zach <Zach@DESKTOP-RKS5BJQ>
2023-05-29 00:08:43 -0500

    Add project files.

---
+++ SchefflerLab_Project/frmScheffler.Designer.cs
@@ -0,0 +1,374 @@
+﻿
+namespace SchefflerLab_Project
+{
+    partial class frmScheffler
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTheDominator = new System.Windows.Forms.Label();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnOrange = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnIndigo = new System.Windows.Forms.Button();
+            this.btnDigit1 = new System.Windows.Forms.Button();
+            this.btnDigit2 = new System.Windows.Forms.Button();
+            this.btnDigit3 = new System.Windows.Forms.Button();
+            this.btnDigit4 = new System.Windows.Forms.Button();
+            this.btnDigit5 = new System.Windows.Forms.Button();
+            this.btnDigit6 = new System.Windows.Forms.Button();
+            this.btnDigit7 = new System.Windows.Forms.Button();
+            this.btnDigit8 = new System.Windows.Forms.Button();
+            this.btnDigit9 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnPink = new System.Windows.Forms.Button();
+            this.btnCyan = new System.Windows.Forms.Button();
+            this.btnBrown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTheDominator
+            // 
+            this.lblTheDominator.BackColor = System.Drawing.Color.White;
+            this.lblTheDominator.Font = new System.Drawing.Font("Times New Roman", 125.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTheDominator.Location = new System.Drawing.Point(12, 195);
+            this.lblTheDominator.Name = "lblTheDominator";
+            this.lblTheDominator.Size = new System.Drawing.Size(460, 222);
+            this.lblTheDominator.TabIndex = 0;
+            this.lblTheDominator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(12, 12);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(136, 56);
+            this.btnRed.TabIndex = 1;
+            this.btnRed.TabStop = false;
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnOrange
+            // 
+            this.btnOrange.BackColor = System.Drawing.Color.Orange;
+            this.btnOrange.Location = new System.Drawing.Point(12, 74);
+            this.btnOrange.Name = "btnOrange";
+            this.btnOrange.Size = new System.Drawing.Size(136, 56);
+            this.btnOrange.TabIndex = 2;
+            this.btnOrange.TabStop = false;
+            this.btnOrange.UseVisualStyleBackColor = false;
+            this.btnOrange.Click += new System.EventHandler(this.btnOrange_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.Location = new System.Drawing.Point(12, 136);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(136, 56);
+            this.btnYellow.TabIndex = 3;
+            this.btnYellow.TabStop = false;
+            this.btnYellow.UseVisualStyleBackColor = false;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.Green;
+            this.btnGreen.Location = new System.Drawing.Point(169, 12);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(136, 56);
+            this.btnGreen.TabIndex = 4;
+            this.btnGreen.TabStop = false;
+            this.btnGreen.UseVisualStyleBackColor = false;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.Location = new System.Drawing.Point(169, 74);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(136, 56);
+            this.btnBlue.TabIndex = 5;
+            this.btnBlue.TabStop = false;
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnIndigo
+            // 
+            this.btnIndigo.BackColor = System.Drawing.Color.Indigo;
+            this.btnIndigo.Location = new System.Drawing.Point(169, 136);
+            this.btnIndigo.Name = "btnIndigo";
+            this.btnIndigo.Size = new System.Drawing.Size(136, 56);
+            this.btnIndigo.TabIndex = 6;
+            this.btnIndigo.TabStop = false;
+            this.btnIndigo.UseVisualStyleBackColor = false;
+            this.btnIndigo.Click += new System.EventHandler(this.btnIndigo_Click);
+            // 
+            // btnDigit1
+            // 
+            this.btnDigit1.BackColor = System.Drawing.Color.Maroon;
+            this.btnDigit1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDigit1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDigit1.Location = new System.Drawing.Point(12, 420);
+            this.btnDigit1.Name = "btnDigit1";
+            this.btnDigit1.Size = new System.Drawing.Size(136, 88);
+            this.btnDigit1.TabIndex = 7;
+            this.btnDigit1.TabStop = false;
+            this.btnDigit1.Text = "1";
+            this.btnDigit1.UseVisualStyleBackColor = false;
+            this.btnDigit1.Click += new System.EventHandler(this.btnDigit1_Click);
+            // 
+            // btnDigit2
+            // 
+            this.btnDigit2.BackColor = System.Drawing.Color.Maroon;
+            this.btnDigit2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDigit2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDigit2.Location = new System.Drawing.Point(169, 420);
+            this.btnDigit2.Name = "btnDigit2";
+            this.btnDigit2.Size = new System.Drawing.Size(136, 88);
+            this.btnDigit2.TabIndex = 8;
+            this.btnDigit2.TabStop = false;
+            this.btnDigit2.Text = "2";
+            this.btnDigit2.UseVisualStyleBackColor = false;
+            this.btnDigit2.Click += new System.EventHandler(this.btnDigit2_Click);
+            // 
+            // btnDigit3
+            // 
+            this.btnDigit3.BackColor = System.Drawing.Color.Maroon;
+            this.btnDigit3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDigit3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDigit3.Location = new System.Drawing.Point(330, 420);
+            this.btnDigit3.Name = "btnDigit3";
+            this.btnDigit3.Size = new System.Drawing.Size(142, 88);
+            this.btnDigit3.TabIndex = 9;
+            this.btnDigit3.TabStop = false;
+            this.btnDigit3.Text = "3";
+            this.btnDigit3.UseVisualStyleBackColor = false;
+            this.btnDigit3.Click += new System.EventHandler(this.btnDigit3_Click);
+            // 
+            // btnDigit4
+            // 
+            this.btnDigit4.BackColor = System.Drawing.Color.Maroon;
+            this.btnDigit4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDigit4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDigit4.Location = new System.Drawing.Point(12, 514);
+            this.btnDigit4.Name = "btnDigit4";
+            this.btnDigit4.Size = new System.Drawing.Size(136, 88);
+            this.btnDigit4.TabIndex = 10;
+            this.btnDigit4.TabStop = false;
+            this.btnDigit4.Text = "4";
+            this.btnDigit4.UseVisualStyleBackColor = false;
+            this.btnDigit4.Click += new System.EventHandler(this.btnDigit4_Click);
+            // 
+            // btnDigit5
+            // 
+            this.btnDigit5.BackColor = System.Drawing.Color.Maroon;
+            this.btnDigit5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDigit5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDigit5.Location = new System.Drawing.Point(169, 514);
+            this.btnDigit5.Name = "btnDigit5";
+            this.btnDigit5.Size = new System.Drawing.Size(136, 88);
+            this.btnDigit5.TabIndex = 11;
+            this.btnDigit5.TabStop = false;
+            this.btnDigit5.Text = "5";
+            this.btnDigit5.UseVisualStyleBackColor = false;
+            this.btnDigit5.Click += new System.EventHandler(this.btnDigit5_Click);
+            // 
+            // btnDigit6
+            // 
+            this.btnDigit6.BackColor = System.Drawing.Color.Maroon;
+            this.btnDigit6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDigit6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDigit6.Location = new System.Drawing.Point(330, 514);
+            this.btnDigit6.Name = "btnDigit6";
+            this.btnDigit6.Size = new System.Drawing.Size(142, 88);
+            this.btnDigit6.TabIndex = 12;
+            this.btnDigit6.TabStop = false;
+            this.btnDigit6.Text = "6";
+            this.btnDigit6.UseVisualStyleBackColor = false;
+            this.btnDigit6.Click += new System.EventHandler(this.btnDigit6_Click);
+            // 
+            // btnDigit7
+            // 
+            this.btnDigit7.BackColor = System.Drawing.Color.Maroon;
+            this.btnDigit7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDigit7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDigit7.Location = new System.Drawing.Point(12, 608);
+            this.btnDigit7.Name = "btnDigit7";
+            this.btnDigit7.Size = new System.Drawing.Size(136, 88);
+            this.btnDigit7.TabIndex = 13;
+            this.btnDigit7.TabStop = false;
+            this.btnDigit7.Text = "7";
+            this.btnDigit7.UseVisualStyleBackColor = false;
+            this.btnDigit7.Click += new System.EventHandler(this.btnDigit7_Click);
+            // 
+            // btnDigit8
+            // 
+            this.btnDigit8.BackColor = System.Drawing.Color.Maroon;
+            this.btnDigit8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDigit8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDigit8.Location = new System.Drawing.Point(169, 608);
+            this.btnDigit8.Name = "btnDigit8";
+            this.btnDigit8.Size = new System.Drawing.Size(136, 88);
+            this.btnDigit8.TabIndex = 14;
+            this.btnDigit8.TabStop = false;
+            this.btnDigit8.Text = "8";
+            this.btnDigit8.UseVisualStyleBackColor = false;
+            this.btnDigit8.Click += new System.EventHandler(this.btnDigit8_Click);
+            // 
+            // btnDigit9
+            // 
+            this.btnDigit9.BackColor = System.Drawing.Color.Maroon;
+            this.btnDigit9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDigit9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDigit9.Location = new System.Drawing.Point(330, 608);
+            this.btnDigit9.Name = "btnDigit9";
+            this.btnDigit9.Size = new System.Drawing.Size(142, 88);
+            this.btnDigit9.TabIndex = 15;
+            this.btnDigit9.TabStop = false;
+            this.btnDigit9.Text = "9";
+            this.btnDigit9.UseVisualStyleBackColor = false;
+            this.btnDigit9.Click += new System.EventHandler(this.btnDigit9_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.White;
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.Location = new System.Drawing.Point(12, 702);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(233, 47);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnExit.Location = new System.Drawing.Point(251, 702);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(221, 47);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnPink
+            // 
+            this.btnPink.BackColor = System.Drawing.Color.Violet;
+            this.btnPink.Location = new System.Drawing.Point(330, 12);
+            this.btnPink.Name = "btnPink";
+            this.btnPink.Size = new System.Drawing.Size(136, 56);
+            this.btnPink.TabIndex = 18;
+            this.btnPink.TabStop = false;
+            this.btnPink.UseVisualStyleBackColor = false;
+            this.btnPink.Click += new System.EventHandler(this.btnPink_Click);
+            // 
+            // btnCyan
+            // 
+            this.btnCyan.BackColor = System.Drawing.Color.Cyan;
+            this.btnCyan.Location = new System.Drawing.Point(330, 74);
+            this.btnCyan.Name = "btnCyan";
+            this.btnCyan.Size = new System.Drawing.Size(136, 56);
+            this.btnCyan.TabIndex = 19;
+            this.btnCyan.TabStop = false;
+            this.btnCyan.UseVisualStyleBackColor = false;
+            this.btnCyan.Click += new System.EventHandler(this.btnCyan_Click);
+            // 
+            // btnBrown
+            // 
+            this.btnBrown.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnBrown.Location = new System.Drawing.Point(330, 136);
+            this.btnBrown.Name = "btnBrown";
+            this.btnBrown.Size = new System.Drawing.Size(136, 56);
+            this.btnBrown.TabIndex = 20;
+            this.btnBrown.TabStop = false;
+            this.btnBrown.UseVisualStyleBackColor = false;
+            this.btnBrown.Click += new System.EventHandler(this.btnBrown_Click);
+            // 
+            // frmScheffler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(484, 761);
+            this.Controls.Add(this.btnBrown);
+            this.Controls.Add(this.btnCyan);
+            this.Controls.Add(this.btnPink);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDigit9);
+            this.Controls.Add(this.btnDigit8);
+            this.Controls.Add(this.btnDigit7);
+            this.Controls.Add(this.btnDigit6);
+            this.Controls.Add(this.btnDigit5);
+            this.Controls.Add(this.btnDigit4);
+            this.Controls.Add(this.btnDigit3);
+            this.Controls.Add(this.btnDigit2);
+            this.Controls.Add(this.btnDigit1);
+            this.Controls.Add(this.btnIndigo);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnOrange);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.lblTheDominator);
+            this.Name = "frmScheffler";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scheffler-Programming Lab";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTheDominator;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnOrange;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnIndigo;
+        private System.Windows.Forms.Button btnDigit1;
+        private System.Windows.Forms.Button btnDigit2;
+        private System.Windows.Forms.Button btnDigit3;
+        private System.Windows.Forms.Button btnDigit4;
+        private System.Windows.Forms.Button btnDigit5;
+        private System.Windows.Forms.Button btnDigit6;
+        private System.Windows.Forms.Button btnDigit7;
+        private System.Windows.Forms.Button btnDigit8;
+        private System.Windows.Forms.Button btnDigit9;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnPink;
+        private System.Windows.Forms.Button btnCyan;
+        private System.Windows.Forms.Button btnBrown;
+    }
+}
+
---
+++ SchefflerLab_Project/frmScheffler.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/* Zach Scheffler
+   Obj-Oriented Programming / ITD-2343
+   30 May, 2023 */
+
+namespace SchefflerLab_Project
+{
+    public partial class frmScheffler : Form
+    {
+        public frmScheffler()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.BackColor = btnRed.BackColor;
+            lblTheDominator.ForeColor = Color.White;
+
+            //background as red, text as white
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.BackColor = btnGreen.BackColor;
+            lblTheDominator.ForeColor = Color.White;
+
+            //background as green, text as white
+        }
+
+        private void btnPink_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.BackColor = btnPink.BackColor;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //background as pink, text as black
+        }
+
+        private void btnOrange_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.BackColor = btnOrange.BackColor;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //background as organe, text as black
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.BackColor = btnBlue.BackColor;
+            lblTheDominator.ForeColor = Color.White;
+
+            //background as blue, text as white
+        }
+
+        private void btnCyan_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.BackColor = btnCyan.BackColor;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //background as cyan, text as black
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.BackColor = btnYellow.BackColor;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //background as yellow, text as black
+        }
+
+        private void btnIndigo_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.BackColor = btnIndigo.BackColor;
+            lblTheDominator.ForeColor = Color.White;
+
+            //background as indigo, text as white
+        }
+
+        private void btnBrown_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.BackColor = btnBrown.BackColor;
+            lblTheDominator.ForeColor = Color.White;
+
+            //background as brown, text as white
+        }
+
+        private void btnDigit1_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = btnDigit1.Text;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //change text to digit 1, text is black if no color button selected
+        }
+
+        private void btnDigit2_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = btnDigit2.Text;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //change text to digit 2, text is black if no color button selected
+        }
+
+        private void btnDigit3_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = btnDigit3.Text;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //change text to digit 3, text is black if no color button selected
+        }
+
+        private void btnDigit4_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = btnDigit4.Text;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //change text to digit 4, text is black if no color button selected
+        }
+
+        private void btnDigit5_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = btnDigit5.Text;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //change text to digit 5, text is black if no color button selected
+        }
+
+        private void btnDigit6_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = btnDigit6.Text;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //change text to digit 6, text is black if no color button selected
+        }
+
+        private void btnDigit7_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = btnDigit7.Text;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //change text to digit 7, text is black if no color button selected
+        }
+
+        private void btnDigit8_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = btnDigit8.Text;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //change text to digit 8, text is black if no color button selected
+        }
+
+        private void btnDigit9_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = btnDigit9.Text;
+            lblTheDominator.ForeColor = Color.Black;
+
+            //change text to digit 9, text is black if no color button selected
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblTheDominator.Text = "";
+            lblTheDominator.BackColor = Color.White;
+
+            //clear out text in label, return to default background color
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+            //close the application
+        }
+    }
+}
